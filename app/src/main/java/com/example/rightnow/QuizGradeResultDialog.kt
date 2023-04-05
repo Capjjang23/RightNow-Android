eb879423c14ac1e6a60d387d68231b6bd70a59a1
mmihye <smilehye910@naver.com>
2023-04-05 21:30:43 +0900

    add quiz grade result dialog

---
+++ app/src/main/java/com/example/rightnow/QuizGradeResultDialog.kt
@@ -0,0 +1,24 @@
+package com.example.rightnow
+
+import com.example.rightnow.databinding.DialogQuizResultBinding
+
+class QuizGradeResultDialog: BaseDialogFragment<DialogQuizResultBinding>(R.layout.dialog_quiz_result) {
+
+    override fun initStartView() {
+        super.initStartView()
+    }
+
+    override fun initDataBinding() {
+        super.initDataBinding()
+
+
+    }
+
+
+    override fun initAfterBinding() {
+        super.initAfterBinding()
+
+
+    }
+
+}
