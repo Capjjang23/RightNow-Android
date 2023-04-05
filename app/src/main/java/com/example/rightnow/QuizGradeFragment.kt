d67b80ceb3885cc972f9a5a34b90a82c750f57ab
mmihye <smilehye910@naver.com>
2023-04-05 21:30:24 +0900

    add quiz grade fragment

---
+++ app/src/main/java/com/example/rightnow/QuizGradeFragment.kt
@@ -0,0 +1,25 @@
+package com.example.rightnow
+
+import com.android.example.travalue.base.BaseFragment
+import com.example.rightnow.databinding.FragmentQuizGradeBinding
+
+class QuizGradeFragment: BaseFragment<FragmentQuizGradeBinding>(R.layout.fragment_quiz_grade) {
+
+    override fun initStartView() {
+        super.initStartView()
+    }
+
+    override fun initDataBinding() {
+        super.initDataBinding()
+
+
+    }
+
+
+    override fun initAfterBinding() {
+        super.initAfterBinding()
+
+
+    }
+
+}
