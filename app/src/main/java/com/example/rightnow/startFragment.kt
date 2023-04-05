ae6031c3f49c6f617852a91321417dd53ef86696
mmihye <smilehye910@naver.com>
2023-04-05 21:27:41 +0900

    add start fragment

---
+++ app/src/main/java/com/example/rightnow/startFragment.kt
@@ -0,0 +1,26 @@
+package com.example.rightnow
+
+import com.android.example.travalue.base.BaseFragment
+import com.example.rightnow.databinding.FragmentQuizBinding
+import com.example.rightnow.databinding.FragmentStartBinding
+
+class startFragment  : BaseFragment<FragmentStartBinding>(R.layout.fragment_start) {
+
+    override fun initStartView() {
+        super.initStartView()
+    }
+
+    override fun initDataBinding() {
+        super.initDataBinding()
+
+
+    }
+
+
+    override fun initAfterBinding() {
+        super.initAfterBinding()
+
+
+    }
+
+}
