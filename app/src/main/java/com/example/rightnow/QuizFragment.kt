e63a7e7f98e30cef86dee5987194ac2e08aab2ed
mmihye <smilehye910@naver.com>
2023-04-05 21:24:27 +0900

    add quiz fragment

--- app/src/main/java/com/example/rightnow/QuizFragment.kt
+++ app/src/main/java/com/example/rightnow/QuizFragment.kt
@@ -1,4 +1,25 @@
 package com.example.rightnow
 
-class QuizFragment {
+import com.android.example.travalue.base.BaseFragment
+import com.example.rightnow.databinding.FragmentQuizBinding
+
+
+class QuizFragment  : BaseFragment<FragmentQuizBinding>(R.layout.fragment_quiz) {
+
+    override fun initStartView() {
+        super.initStartView()
+    }
+
+    override fun initDataBinding() {
+        super.initDataBinding()
+
+
+    }
+
+
+    override fun initAfterBinding() {
+        super.initAfterBinding()
+
+
+    }
 }
